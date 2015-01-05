19e6fbee3ed167c2959028e994fb6b3cd76d5b52
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2015-01-05 12:35:44 +0200

    Light modelling, filtering, looking at residuals.

--- preprocess2.R
+++ preprocess2.R
@@ -4,26 +4,37 @@ library(tidyr)
 t0 <- read.table("data/v3.mean_GISS_homogenized_cleaned.txt", header=T, sep=";") %>% tbl_df()
 s0 <- read.table("data/station_list_cleaned.txt", header=T, sep=";") %>% tbl_df()
 
-sids <- s0 %>% filter(lat>58 & lat<80 & lon>20 & lon<38) %>% .$sid
+sids <- s0 %>% filter(lat>55 & lat<80 & lon>12 & lon<38) %>% .$sid
 t2 <- t0 %>% filter(sid %in% sids) %>% 
   gather(month, t, Jan:Dec) %>% 
-  filter(t>-9990) %>%
+  filter(t>-9990 & year>1980) %>%
   left_join(s0, by="sid") %>%
-  mutate(sid=as.factor(sid), imonth=as.numeric(month), t=t/10) %>%
-  filter(year>1780 & sid != 10490000)  # Alta lufthavn is broken
+  mutate(sid=as.factor(sid), 
+         subsid=as.factor(paste(sid, sub, sep=".")), name=as.factor(paste(name, sub, sep=":")),
+         imonth=as.numeric(month), t=t/10) 
+if (F) {
+  t2 %>%  ggplot(aes(x=year, y=t, color=name)) + geom_line() + facet_wrap(~ month)
+  m <- lmer(t ~ (1 + decade | subsid) + (1 + decade | month) + decade, 
+            data=t2 %>% filter(year>1980) %>% mutate(decade=(year-mean(year))/10))
+  ranef(m)$subsid["(Intercept)"] # Outliers
+  t2 %>% group_by(subsid) %>% summarise(n=n()) %>% filter(n<12*5) # Almost no obs
+}
+
+t2 <- t2 %>% filter(! (subsid %in% c("10490000.1336", "10920000.4037", "22260000.4257", "10920000.6340")) )
+t2 %>%  ggplot(aes(x=year, y=t, color=name)) + geom_line() + facet_wrap(~ month)
 
 if (F) {
   y <- t2 %>% group_by(year) %>% summarise(n=n())
   with(y, plot(year, n/12, type="l"))
   hist(t2$t, n=100)
-  library(mgcv)
-  plot(gam(t ~ sid + s(imonth) + s(year), data=t2), scale=F)
-  plot(gam(t ~ sid + s(imonth) + s(year), data=t2 %>% filter(year>1980)), scale=F)
-  library(lme4)
-  m <- lmer(t ~ (1 + decade | name) + (1 + decade | month) + decade, 
-            data=t2 %>% filter(year>1980) %>% mutate(decade=(year-mean(year))/10))
-  summary(m)
-  ranef(m)
 }
 
-library(mgcv)
+m <- lmer(t ~ (1 + decade | name) + (1 + decade | month) + decade, 
+          data=t2 %>% mutate(decade=(year-mean(year))/10))
+summary(m)
+ranef(m)
+t2 %>%  ggplot(aes(x=year, y=t, color=name)) + geom_line() + facet_wrap(~ month)
+t2 %>% mutate(r=resid(m)) %>% ggplot(aes(x=year, y=r, color=name)) + geom_point() + facet_wrap(~ month)
+t2 %>% mutate(r=resid(m)) %>% ggplot(aes(x=year, y=r, color=month)) + geom_point() + facet_wrap(~ name)
+# There's significant name-month interaction
+# Monthly residuals are different
