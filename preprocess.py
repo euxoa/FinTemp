a74afc69af833001243ce125cfbadfddae0e768c
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2015-01-05 12:35:23 +0200

    Substation, remove spaces from names.

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,7 @@ for line in ifile:
     g = re.match("^([0-9]+) (.*) lat,lon \(\.1deg\) +([0-9-]+) +([0-9-]+) .*$", line)
     if g:
         sid, name, slat, slon = g.groups()
-        name = name.strip()
+        name = re.sub(" +", " ", name.strip())
         lat, lon = float(slat)/10, float(slon)/10
         print >>ofile, ";".join((sid, '"%s"'%name, '%s'%lat, '%s'%lon))
 
@@ -18,6 +18,6 @@ for line in ifile:
 # ifile = urllib2.urlopen("http://data.giss.nasa.gov/gistemp/station_data/v3.mean_GISS_homogenized.txt.gz")
 ifile = open("data/v3.mean_GISS_homogenized.txt")
 ofile = open("data/v3.mean_GISS_homogenized_cleaned.txt", 'w')
-print >>ofile, "sid;year;Jan;Feb;Mar;Apr;May;Jun;Jul;Aug;Sep;Oct;Nov;Dec"
+print >>ofile, "sub;sid;year;Jan;Feb;Mar;Apr;May;Jun;Jul;Aug;Sep;Oct;Nov;Dec"
 for line in ifile:
-    print >>ofile, ";".join([line[4:12]]+[line[11+5*i:16+5*i].strip() for i in xrange(13)])
+    print >>ofile, ";".join([line[0:4], line[4:12]] + [line[11+5*i:16+5*i].strip() for i in xrange(13)])
