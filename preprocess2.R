9e52557c4dabd5ca5d4e815c559aa7b0a9681fa2
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2015-01-09 21:02:37 +0200

    Remove very incomplete stations.

--- preprocess2.R
+++ preprocess2.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
 t0 <- read.table("data/v3.mean_GISS_homogenized_cleaned.txt", header=T, sep=";") %>% tbl_df()
 s0 <- read.table("data/station_list_cleaned.txt", header=T, sep=";") %>% tbl_df()
@@ -12,15 +13,20 @@ t2 <- t0 %>% filter(sid %in% sids) %>%
   mutate(sid=as.factor(sid), 
          subsid=as.factor(paste(sid, sub, sep=".")), name=as.factor(paste(name, sub, sep=":")),
          imonth=as.numeric(month), t=t/10, decade=(year-mean(year))/10)
+
 if (F) {
   t2 %>%  ggplot(aes(x=year, y=t, color=name)) + geom_line() + facet_wrap(~ month)
   m <- lmer(t ~ (1 + decade | subsid) + (1 + decade | month) + decade, data=t2)
   ranef(m)$subsid["(Intercept)"] # Outliers
-  t2 %>% group_by(subsid) %>% summarise(n=n()) %>% filter(n<12*5) # Almost no obs
+  t2 %>% group_by(subsid) %>% summarise(n=n()) %>% filter(n<12*20) # Almost no obs
 }
 
+too.short <- (t2 %>% group_by(subsid) %>% summarise(n=n()) %>% filter(n<12*15))$subsid
 t2 <- t2 %>% filter(! (subsid %in% c("10490000.1336", "10920000.4037", "22260000.4257", "10920000.6340")) )
-t2 %>%  ggplot(aes(x=year, y=t, color=name)) + geom_line() + facet_wrap(~ month)
+t2 <- t2 %>% filter(! (subsid %in% too.short))
+t2 %>%  ggplot(aes(x=year, y=t, color=name)) + geom_point() + facet_wrap(~ month)
+t2 %>%  ggplot(aes(x=year, y=t, color=month)) + geom_point() + facet_wrap(~ name)
+
 
 if (F) {
   y <- t2 %>% group_by(year) %>% summarise(n=n())
@@ -46,6 +52,9 @@ fixef(m4)["I(lat - mean(lat)):decade"]
 ranef(m4)$month
 t2 %>% mutate(r=resid(m4)) %>% ggplot(aes(x=year, y=r, color=name)) + geom_point() + facet_wrap(~ month)
 t2 %>% mutate(r=resid(m4)) %>% ggplot(aes(x=year, y=r, color=month)) + geom_point() + facet_wrap(~ name)
+acf((t2 %>% mutate(r=resid(m4)) %>% group_by(year, imonth) %>% 
+       summarise(r=mean(r)) %>% mutate(ym=year+imonth/12) %>% arrange(ym))$r)
+
 
 # library(gamm4)
 m5 <- gam(t ~ s(name, month, bs="re") + s(I(imonth+.5), k=12, by=decade, bs="cc") + (I(lat-62) + I(lon-29)):decade, 
